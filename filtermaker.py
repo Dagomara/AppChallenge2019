98c08ce3a4f1b6d21544c36b73445b3dab459e24
Dagomara <53799719+Dagomara@users.noreply.github.com>
2019-10-21 14:09:25 -0400

    Filter Maker
    Generates filters for the Java NN's uses. Basically generates a whole bunch of gradients.

--- filtermaker.py
+++ filtermaker.py
@@ -21,25 +21,28 @@ for layer in range(lcount-1):
                 filt.append(math.randint(0, 30) + " ")
             f.write(filt)
             """
-filters = input("How many filters? ")
-w = int(input("width? "))
-h = int(input("height? "))
+layers = input("How many layers? ")
+filters = input("How many filters per layer? ")
+w = int(input("filter width? "))
+h = int(input("filter height? "))
 minimum = 20
 maximum = 40
 rounding=1000.00
-
-f.write(str(filters) + "\n")
-for filter in range(filters):
-    f.write( str(w) + " " + str(h) + "\n")
-    for y in range(h):
-        filt = ""
-        for x in range(w):
-            val = ( (random.random()*maximum)-abs(minimum) )
-            valFixed = float(int(rounding * val)/rounding)
-            filt += ((str(valFixed) + " "))
-        filt +="\n"
-        print(filt)
-        f.write(filt)
+f.write(str(layers) + "\n")
+for layer in range(layers):
+    f.write("Layer " + str(layer+1) + "\n")
+    f.write(str(filters) + "\n")
+    for filter in range(filters):
+        f.write( str(w) + " " + str(h) + "\n")
+        for y in range(h):
+            filt = ""
+            for x in range(w):
+                val = ( (random.random()*(maximum-minimum))+abs(minimum) )
+                valFixed = float(int(rounding * val)/rounding)
+                filt += ((str(valFixed) + " "))
+            filt +="\n"
+            print(filt)
+            f.write(filt)
 
 
 f.close()
